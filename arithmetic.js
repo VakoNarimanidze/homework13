28fe0b4479c1fd5a0c4e2148a140bb434532a022
VakoNarimanidze <vako.narimanidze5@gmail.com>
2023-01-16 16:54:44 +0400

    arithmetic calculate

---
+++ arithmetic.js
@@ -0,0 +1,12 @@
+let x = 5;
+let y = 10;
+
+let sum = x + y;
+let div = x * y;
+let minus = y - x
+let division = y / x
+
+ console.log(sum)
+ console.log(div)
+ console.log(minus)
+ console.log(division)
